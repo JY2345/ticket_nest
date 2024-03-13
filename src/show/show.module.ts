2d2b6ed78cb9cc70f04e27c96853a190af0d36e2
JY2345 <ilokids@gmail.com>
2024-03-13 12:16:58 +0900

    Feat : create show

--- src/show/show.module.ts
+++ src/show/show.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Show, Seat, ShowDate } from './entities/show.entity';
 import { ShowService } from './show.service';
 import { ShowController } from './show.controller';
 
 @Module({
-  controllers: [ShowController],
+  imports: [TypeOrmModule.forFeature([Show, Seat, ShowDate])],
   providers: [ShowService],
+  controllers: [ShowController],
 })
 export class ShowModule {}
--- src/show/show.service.ts
+++ src/show/show.service.ts
@@ -1,11 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Show, Seat } from './entities/show.entity';
 import { CreateShowDto } from './dto/create-show.dto';
 import { UpdateShowDto } from './dto/update-show.dto';
 
 @Injectable()
 export class ShowService {
-  create(createShowDto: CreateShowDto) {
-    return 'This action adds a new show';
+  constructor(
+    @InjectRepository(Show)
+    private showRepository: Repository<Show>,
+    @InjectRepository(Seat)
+    private seatRepository: Repository<Seat>,
+  ) {}
+
+  async create(createShowDto: CreateShowDto): Promise<Show> {
+    const show = this.showRepository.create({
+      ...createShowDto,
+      seats: [],
+    });
+
+    const seats = createShowDto.seats.map((seatDto) => {
+      let seat = this.seatRepository.create({
+        ...seatDto,
+        show: show,
+      });
+      return seat;
+    });
+    show.seats = await this.seatRepository.save(seats);
+
+    await this.showRepository.save(show);
+    return show;
   }
 
   findAll() {
